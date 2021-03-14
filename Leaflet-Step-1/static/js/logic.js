6cea61e716ac3f11cb4f9f06d99a21ed37ae9f2d
kehull <71107280+kehull@users.noreply.github.com>
2021-03-13 19:40:41 -0600

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -24,29 +24,27 @@ var myMap = L.map("mapid", {
 
     for (var i = 0; i < response.features.length; i++) {
   
-            // Set the variables
-            var lat = response.features[i].geometry.coordinates[1]
-            var long = response.features[i].geometry.coordinates[0]
-            var depth = response.features[i].geometry.coordinates[2]
-            var size = response.features[i].properties.mag
-            var loc = response.features[i].properties.place
+      // Set the variables
+      var lat = response.features[i].geometry.coordinates[1]
+      var long = response.features[i].geometry.coordinates[0]
+      var depth = response.features[i].geometry.coordinates[2]
+      var size = response.features[i].properties.mag
+      var loc = response.features[i].properties.place
       
-            depthInfo.push(Number(depth))
-        
-            // Check for location property
-            if (lat && long && depth && size) {
-              var location = [lat, long]
-              var color = "#fff"
-              // create marker
-              L.circle(location, {
-              fillOpacity: 0.5,
-              color: "white",
-              fillColor: color,
-              // Adjust radius
-              radius: size*24000
-              }).bindPopup("<h1>" + loc + "</h1> <hr> <h3>Magnitude: " + size + "</h3> <hr> <h3>Depth: " + depth + "</h3>").addTo(myMap);  
-              // WRITE IF/ELSE STATEMENTS TO CHANGE MARKER COLOR/SIZE BASED ON PARAMETERS
-          }
+      // Check for location property
+      if (lat && long && depth && size) {
+        var location = [lat, long]
+        var color = "#fff"
+        // create marker
+        L.circle(location, {
+        fillOpacity: 0.5,
+        color: "white",
+        fillColor: color,
+        // Adjust radius
+        radius: size*24000
+        }).bindPopup("<h1>" + loc + "</h1> <hr> <h3>Magnitude: " + size + "</h3> <hr> <h3>Depth: " + depth + "</h3>").addTo(myMap);  
+        // WRITE IF/ELSE STATEMENTS TO CHANGE MARKER COLOR/SIZE BASED ON PARAMETERS
+    }
 
 
     // Create a new choropleth layer
